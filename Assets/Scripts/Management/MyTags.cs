344c82c99aac72ffc26b47b4bdf299d7d17c14d4
weetman <pnunes91@gmail.com>
2017-04-02 23:33:12 -0300

    Unity Project
    Unity project with the main scene set up, ready to start working.

---
+++ Assets/Scripts/Management/MyTags.cs
@@ -0,0 +1,9 @@
+﻿public static class MyTags {
+
+	public static string respawn = "Respawn";
+	public static string finish = "Finish";
+	public static string editorOnly = "EditorOnly";
+	public static string mainCamera = "MainCamera";
+	public static string player = "Player";
+	public static string gameController = "GameController";
+}
---
+++ Assets/Scripts/StartLevelManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartLevelManager : MonoBehaviour {
+
+	public void LoadLevel (string levelName){
+		SceneManager.LoadScene (levelName);
+	}
+}
