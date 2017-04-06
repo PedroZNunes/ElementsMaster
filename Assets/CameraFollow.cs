f5061fd3488c71cac81606d90eb510055dd51161
weetman <pnunes91@gmail.com>
2017-04-06 16:25:00 -0300

    Adjusting camera settings.

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,26 +4,86 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
+    enum CameraState {Moving, Idle};
+    CameraState cameraState;
+
     [SerializeField]
     float verticalOffset;
-
     [SerializeField]
     Controller2D target;
     [SerializeField]
     Vector2 focusAreaSize;
 
     FocusArea focusArea;
+    Coroutine cameraIdling;
+
+    [SerializeField]
+    LookAhead lookAhead;
+    [SerializeField]
+    Smooth smoothIdle;
+    [SerializeField]
+    Smooth smoothMoving;
+    [SerializeField]
+    float idleSeedMin, idleSeedMax;
+    [SerializeField]
+    float smoothChangingTime;
+    
+    Smooth currentSmooth;
 
     void Start () {
         focusArea = new FocusArea (target.collider.bounds , focusAreaSize);
+        cameraState = CameraState.Idle;
     }
+
 
     void LateUpdate () {
         focusArea.Update (target.collider.bounds);
-        //Camera Movement
+        if (Vector3.Magnitude (focusArea.velocity) == 0f && cameraState != CameraState.Idle) {
+            cameraState = CameraState.Idle;
+        }
+        else if (Vector3.Magnitude (focusArea.velocity) > 0f && cameraState != CameraState.Moving) {
+            cameraState = CameraState.Moving;
+        }
         Vector2 focusPosition = focusArea.center + Vector2.up * verticalOffset;
+
+        CalculateMovement (ref focusPosition);
+        
         transform.position = (Vector3)focusPosition + Vector3.forward * -10;
+        
     }
+
+    void HandleCameraMoving (ref Vector2 focusPosition) {
+        Debug.Log ("Camera Moving");
+
+        lookAhead.directionX = Mathf.Sign (focusArea.velocity.x);
+        lookAhead.targetX = lookAhead.directionX * lookAhead.distanceX;
+        lookAhead.currentX = Mathf.SmoothDamp (lookAhead.currentX , lookAhead.targetX , ref smoothMoving.smoothVelocity.x , smoothMoving.smoothTime.x);
+
+        focusPosition += Vector2.right * lookAhead.currentX;
+    }
+
+    void HandleCameraIdle ( ref Vector2 focusPosition ) {
+        Debug.Log ("Camera Idle");
+
+        lookAhead.currentX = Mathf.SmoothDamp (lookAhead.currentX , lookAhead.targetX , ref smoothIdle.smoothVelocity.x , smoothIdle.smoothTime.x);
+
+        focusPosition += Vector2.right * lookAhead.currentX;
+    }
+
+    void CalculateMovement (ref Vector2 focusPosition) {
+        switch (cameraState) {
+            case CameraState.Moving:
+                HandleCameraMoving (ref focusPosition);
+                break;
+            case CameraState.Idle:
+                HandleCameraIdle (ref focusPosition);
+                break;
+            default:
+                cameraState = CameraState.Idle;
+                break;
+        }
+    }
+
 
     void OnDrawGizmos () {
         Gizmos.color = new Color (0 , 1 , 1 , 0.4f);
@@ -60,12 +120,12 @@ public class CameraFollow : MonoBehaviour {
 
             center = new Vector2 (( left + right ) / 2 , ( top + bottom) / 2 );
             velocity = new Vector2 (shiftX , shiftY);
-            print (string.Format ("left: {0}, right: {1}, bottom {2}, top {3}, center: {4}" , left , right , bottom , top, center));
+            //print (string.Format ("left: {0}, right: {1}, bottom {2}, top {3}, center: {4}" , left , right , bottom , top, center));
             
         }
 
         private void CalculateShiftAmounts ( Bounds bounds , ref float shiftX, ref float shiftY) {
-            Debug.Log ("Bounds : " + bounds);
+            //Debug.Log ("Bounds : " + bounds);
             if (bounds.min.x < left) {
                 shiftX = bounds.min.x - left;
             }
@@ -80,5 +140,24 @@ public class CameraFollow : MonoBehaviour {
                 shiftY = bounds.max.y - top;
             }
         }
+    }
+    [System.Serializable]
+    struct LookAhead {
+        [HideInInspector]
+        public float currentX;
+        [HideInInspector]
+        public float targetX;
+        [HideInInspector]
+        public float directionX;
+        
+        public float distanceX;
+        Vector2 velocity;
+    }
+
+    [System.Serializable]
+    struct Smooth {
+        [HideInInspector]
+        public Vector2 smoothVelocity;
+        public Vector2 smoothTime;
     }
 }
